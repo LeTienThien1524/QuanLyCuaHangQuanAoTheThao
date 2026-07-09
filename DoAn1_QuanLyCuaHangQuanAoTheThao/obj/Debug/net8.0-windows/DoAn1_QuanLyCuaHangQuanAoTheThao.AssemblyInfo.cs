d7809b0695345f20f00087b6a4fb3618af9d45a2
Lê Tiến Thiên <letienthien15022004@gmail.com>
2026-07-09 19:44:09 +0700

    Mô tả thay đổi

--- DoAn1_QuanLyCuaHangQuanAoTheThao/obj/Debug/net8.0-windows/DoAn1_QuanLyCuaHangQuanAoTheThao.AssemblyInfo.cs
+++ DoAn1_QuanLyCuaHangQuanAoTheThao/obj/Debug/net8.0-windows/DoAn1_QuanLyCuaHangQuanAoTheThao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAn1_QuanLyCuaHangQuanAoTheThao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724c56d564868000777eafb0b855582fe0b53853")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAn1_QuanLyCuaHangQuanAoTheThao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAn1_QuanLyCuaHangQuanAoTheThao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
